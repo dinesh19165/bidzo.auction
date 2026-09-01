53cb8935041a141290982e570881b8d283fb01fc
dinesh <dineshnaidu009@gmail.com>
2026-09-01 17:53:48 +0530

    api

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,5 +1,5 @@
-//export const API_BASE_URL = 'https://bidzo-backend.onrender.com/api';
-export const API_BASE_URL = 'http://localhost:8080/api';
+export const API_BASE_URL = 'https://bidzo-backend.onrender.com/api';
+//export const API_BASE_URL = 'http://localhost:8080/api';
 
 export class ApiError extends Error {
   status: number;
